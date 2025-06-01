3785fea93ced3b5ed9250a66efe5478c7d54b6b6
Douhenr1 <Douhenr@icloud.com>
2025-05-31 23:10:47 -0300

    Ex009

---
+++ exercicios/ex008.py
@@ -0,0 +1,6 @@
+n1 = int(input("Digite um valor em metros ! "))
+
+cm = n1 * 100
+mm = n1 * 1000
+
+print('O numero {} m tem {} cm e {} mm' .format(n1, cm, mm))
---
+++ exercicios/ex009.py
@@ -0,0 +1,14 @@
+numero1 = int(input("Diogite um numero !"))
+
+n1 = numero1 * 1
+n2 = numero1 * 2
+n3 = numero1 * 3
+n4 = numero1 * 4
+n5 = numero1 * 5
+n6 = numero1 * 6
+n7 = numero1 * 7
+n8 = numero1 * 8
+n9 = numero1 * 9
+n10 = numero1 * 10
+
+print('o valor da tabuada do numero {} é {} {} {} {} {} {} {} {} {} {}' .format(numero1, n1, n2, n3, n4, n5, n6, n7, n8, n9, n10) )
